b4193b01dc10ae60a8f4d6f24bedd52e94ffbd8d
AndreGuima <andrepaivaguimaraes@gmail.com>
2023-10-13 12:23:00 -0300

    first commit

---
+++ src/test/java/br/com/andreguimaraes/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andreguimaraes.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
